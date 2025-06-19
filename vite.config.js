fb6ae6711aa5e2de41e6d38dde20b18f7062f727
Yolanda-bc <jolandabc89@gmail.com>
2025-06-20 01:28:51 +0200

    change package

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://yolanda-bc.github.io/project-married/",
+  base: "/project-married/",
   plugins: [react()],
   server: {
     open: "/",
