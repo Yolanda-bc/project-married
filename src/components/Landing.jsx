f9b604db01b8612e22e0314425762b6f55088f49
Yolanda-bc <jolandabc89@gmail.com>
2025-09-22 11:55:51 +0200

    add image

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import qr_image from "../images/qr-code2.png";
+import qr_image from "../images/qr-code-2.png";
 
 import "../styles/core/_landing.scss";
 import { Link } from "react-router-dom";
